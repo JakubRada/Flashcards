98906d5c35740a91a07cdb4c4eebd2298f7154eb
JakubRada <jacub.rada@gmail.com>
2019-01-24 22:51:11 +0100

    fixed bug when buttons linked to title_page -> event.preventDefault()

--- user_interface/actions.js
+++ user_interface/actions.js
@@ -20,7 +20,6 @@ function hideAll() {
     for (var i = 0; i < controlButtons.length; i += 1) {
         $('#' + controlButtons[i]).hide();
     };
-    console.log('everything hidden');
 };
 
 function reset() {
@@ -46,36 +45,45 @@ function oneChoice(choice_num) {
 $(document).ready(function() {
     reset();
     // initializing clicks on navigation bar
-    $('#home_button').click( function() {
+    $('#home_button').click( function(event) {
+        event.preventDefault();
         reset();
     });
-    $('#create_card_button').click( function() {
+    $('#create_card_button').click( function(event) {
+        event.preventDefault();
         showOneItem('create_card');
         $('#card_created').hide();
     });
-    $('#edit_card_button').click( function() {
+    $('#edit_card_button').click( function(event) {
+        event.preventDefault();
         showOneItem('card_list');
     });
-    $('#create_tag_button').click( function() {
+    $('#create_tag_button').click( function(event) {
+        event.preventDefault();
         showOneItem('create_tag');
         $('#tag_created').hide();
     });
-    $('#edit_tag_button').click( function() {
+    $('#edit_tag_button').click( function(event) {
+        event.preventDefault();
         showOneItem('tag_list');
     });
-    $('#test_browse_button').click( function() {
+    $('#test_browse_button').click( function(event) {
+        event.preventDefault();
         showOneItem('test_browse');
         $('#browse_window').hide();
-        $('#start_browse_button').click( function() {
+        $('#start_browse_button').click( function(event) {
+            event.preventDefault();
             $('#start_browse').hide();
             $('#browse_window').show();
         });
         $('#start_browse').show();
     });
-    $('#test_choices_button').click( function() {
+    $('#test_choices_button').click( function(event) {
+        event.preventDefault();
         showOneItem('test_choices');
         $('#choices_window').hide();
-        $('#start_choices_button').click( function() {
+        $('#start_choices_button').click( function(event) {
+            event.preventDefault();
             $('#start_choices').hide();
             $('#choices_window').show();
             $('#option_1').click( function() {
@@ -93,19 +101,23 @@ $(document).ready(function() {
         });
         $('#start_choices').show();
     });
-    $('#test_write_button').click( function() {
+    $('#test_write_button').click( function(event) {
+        event.preventDefault();
         showOneItem('test_write');
         $('#write_window').hide();
-        $('#start_write_button').click( function() {
+        $('#start_write_button').click( function(event) {
+            event.preventDefault();
             $('#start_write').hide();
             $('#write_window').show();
         });
         $('#start_write').show();
     });
-    $('#import_button').click( function() {
+    $('#import_button').click( function(event) {
+        event.preventDefault();
         showOneItem('import');
     });
-    $('#export_button').click( function() {
+    $('#export_button').click( function(event) {
+        event.preventDefault();
         showOneItem('export');
     });
     // fliping card on click
