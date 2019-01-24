0c9a296f2bc83fdba04dcfffb0d349407f3c8fa0
JakubRada <jacub.rada@gmail.com>
2019-01-24 20:18:50 +0100

    setting up the most important buttons; browse cards with flip; one bug with reloading on first start of test

--- user_interface/actions.js
+++ user_interface/actions.js
@@ -16,23 +16,101 @@ var controlButtons = new Array(
     'wrong_answer',
 );
 
-function reset() {
+function hideAll() {
     for (var i = 0; i < controlButtons.length; i += 1) {
-        if (controlButtons[i] == 'title_page') {
-            $('#' + controlButtons[i]).show();
-            console.log("shown");
-        } else {
-            $('#' + controlButtons[i]).hide();
-            console.log('hidden');
-        }
-    }
-}
+        $('#' + controlButtons[i]).hide();
+    };
+    console.log('everything hidden');
+};
 
+function reset() {
+    hideAll();
+    $('#title_page').show();
+};
+
+function showOneItem(item) {
+    hideAll();
+    $('#' + item).show();
+};
+
+function oneChoice(choice_num) {
+    for (var i = 1; i <= 4; i += 1) {
+        if (i == choice_num) {
+            $('#option_' + i).addClass('active');
+        } else {
+            $('#option_' + i).removeClass('active');
+        };
+    };
+};
 
 $(document).ready(function() {
     reset();
-    $('#create_card_button').click(function( event ) {
-        event.preventDefault();
-        console.log(123);
+    // initializing clicks on navigation bar
+    $('#home_button').click( function() {
+        reset();
+    });
+    $('#create_card_button').click( function() {
+        showOneItem('create_card');
+        $('#card_created').hide();
+    });
+    $('#edit_card_button').click( function() {
+        showOneItem('card_list');
+    });
+    $('#create_tag_button').click( function() {
+        showOneItem('create_tag');
+        $('#tag_created').hide();
+    });
+    $('#edit_tag_button').click( function() {
+        showOneItem('tag_list');
+    });
+    $('#test_browse_button').click( function() {
+        showOneItem('test_browse');
+        $('#browse_window').hide();
+        $('#start_browse_button').click( function() {
+            $('#start_browse').hide();
+            $('#browse_window').show();
+        });
+        $('#start_browse').show();
+    });
+    $('#test_choices_button').click( function() {
+        showOneItem('test_choices');
+        $('#choices_window').hide();
+        $('#start_choices_button').click( function() {
+            $('#start_choices').hide();
+            $('#choices_window').show();
+            $('#option_1').click( function() {
+                oneChoice(1);
+            });
+            $('#option_2').click( function() {
+                oneChoice(2);
+            });
+            $('#option_3').click( function() {
+                oneChoice(3);
+            });
+            $('#option_4').click( function() {
+                oneChoice(4);
+            });
+        });
+        $('#start_choices').show();
+    });
+    $('#test_write_button').click( function() {
+        showOneItem('test_write');
+        $('#write_window').hide();
+        $('#start_write_button').click( function() {
+            $('#start_write').hide();
+            $('#write_window').show();
+        });
+        $('#start_write').show();
+    });
+    $('#import_button').click( function() {
+        showOneItem('import');
+    });
+    $('#export_button').click( function() {
+        showOneItem('export');
+    });
+    // fliping card on click
+    $('.flip-card .flip-card-inner').click( function() {
+        $(this).closest('.flip-card').toggleClass('hover');
+        $(this).css('transform, rotateX(180deg)');
     });
 });
