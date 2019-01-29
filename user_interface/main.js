ee0ebcb1dc07c2dd2b17a68bee4e2328f5bce9d5
JakubRada <jacub.rada@gmail.com>
2019-01-29 15:25:12 +0100

    file names changed + listing cards from database

--- user_interface/main.js
+++ user_interface/main.js
@@ -1,31 +1,164 @@
-// required dependencies
-const electron = require('electron');
-const url = require('url');
-const path = require('path');
+var controlButtons = new Array(
+    'progress_bar',
+    'title_page',
+    'card_list',
+    'tag_list',
+    'create_card',
+    'create_tag',
+    'edit_card',
+    'edit_tag',
+    'test_browse',
+    'test_choices',
+    'test_write',
+    'import',
+    'export',
+    'correct_answer',
+    'wrong_answer',
+);
 
-const { app, BrowserWindow } = electron;
+function hideAll() {
+    for (var i = 0; i < controlButtons.length; i += 1) {
+        $('#' + controlButtons[i]).hide();
+    };
+};
 
-let mainWindow;
-// waiting for the app to be ready
-app.on('ready', function() {
-    // creating main window
-    mainWindow = new BrowserWindow({
-        width: 1200,
-        minWidth: 800,
-        height: 800,
-        minHeight: 600,
-        title: 'Cards',
+function reset() {
+    hideAll();
+    $('#title_page').show();
+};
+
+function showOneItem(item) {
+    hideAll();
+    $('#' + item).show();
+};
+
+function oneChoice(choice_num) {
+    for (var i = 1; i <= 4; i += 1) {
+        if (i == choice_num) {
+            $('#option_' + i).addClass('active');
+        } else {
+            $('#option_' + i).removeClass('active');
+        };
+    };
+};
+
+function browse() {
+    showOneItem('test_browse');
+    $('#browse_window').hide();
+    $('#start_browse_button').click( function(event) {
+        event.preventDefault();
+        $('#start_browse').hide();
+        $('#progress_bar').show();
+        $('#browse_window').show();
+        // fliping card on click
+        $('.flip-card .flip-card-inner').click( function() {
+            $(this).closest('.flip-card').toggleClass('hover');
+            $(this).css('transform, rotateX(180deg)');
+        });
     });
-    // removes the upper menu bar
-    // mainWindow.setMenu(null);
-    // loading mainWindow.html with correct path
-    mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, 'mainWindow.html'),
-        protocol: 'file',
-        slashes: true
-    }));
-    // Quit app when closed
-    mainWindow.on('closed', function() {
-        app.quit();
+    $('#start_browse').show();
+};
+
+function choices() {
+    showOneItem('test_choices');
+    $('#choices_window').hide();
+    $('#start_choices_button').click( function(event) {
+        event.preventDefault();
+        $('#start_choices').hide();
+        $('#progress_bar').show();
+        $('#choices_window').show();
+        $('#option_1').click( function() {
+            oneChoice(1);
+        });
+        $('#option_2').click( function() {
+            oneChoice(2);
+        });
+        $('#option_3').click( function() {
+            oneChoice(3);
+        });
+        $('#option_4').click( function() {
+            oneChoice(4);
+        });
+    });
+    $('#start_choices').show();
+};
+
+function write() {
+    showOneItem('test_write');
+    $('#write_window').hide();
+    $('#start_write_button').click( function(event) {
+        event.preventDefault();
+        $('#start_write').hide();
+        $('#progress_bar').show();
+        $('#write_window').show();
+    });
+    $('#start_write').show();
+};
+
+function edit_cards() {
+    $("#table_of_cards tbody").empty();
+    load_information("cards").done(function(card_list) {
+        for (var index in card_list) {
+            load_information("cards/" + card_list[index].id).done(function(card_info){
+                $(
+                    '<tr><th scope="row">' + card_info.id + '</th><td>' + card_info.card_front + '</td><td>' + card_info.card_back + '</td><td><span class="badge badge-dark">' + card_info.tag_count + '</span></td><td><button type="button" class="btn btn-warning">Edit</button> <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#delete_conf">Delete</button></td></tr>'
+                ).appendTo("#table_of_cards tbody");
+            });
+        };
+    });
+    showOneItem('card_list');
+};
+
+
+function load_information(suffix) {
+    return $.ajax({
+        url: "http://127.0.0.1:8000/cards/" + suffix,
+    });
+};
+
+$(document).ready(function() {
+    reset();
+    // clicks on navigation bar
+    $('#home_button').click( function(event) {
+        event.preventDefault();
+        reset();
+    });
+    $('#create_card_button').click( function(event) {
+        event.preventDefault();
+        showOneItem('create_card');
+        $('#card_created').hide();
+    });
+    $('#edit_card_button').click( function(event) {
+        event.preventDefault();
+        edit_cards();
+    });
+    $('#create_tag_button').click( function(event) {
+        event.preventDefault();
+        showOneItem('create_tag');
+        $('#tag_created').hide();
+    });
+    $('#edit_tag_button').click( function(event) {
+        event.preventDefault();
+        showOneItem('tag_list');
+    });
+    $('#test_browse_button').click( function(event) {
+        event.preventDefault();
+        browse();
+    });
+    $('#test_choices_button').click( function(event) {
+        event.preventDefault();
+        choices();
+    });
+    $('#test_write_button').click( function(event) {
+        event.preventDefault();
+        write();
+    });
+    $('#import_button').click( function(event) {
+        event.preventDefault();
+        showOneItem('import');
+    });
+    $('#export_button').click( function(event) {
+        event.preventDefault();
+        showOneItem('export');
     });
 });
